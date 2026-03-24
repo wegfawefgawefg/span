7c41bd54519dfd9f78b33379eb54773b73536d4a
kyle dougan <kyle.dougan@e-o.solutions>
2026-03-23 19:30:23 -1000

    remove old Sheet types from types.ts

--- src/mainview/src/types.ts
+++ src/mainview/src/types.ts
@@ -3,17 +3,6 @@ import type { Annotation } from "./annotation";
 
 export type { Annotation };
 
-export interface Sheet {
-	file: string;
-	name: string;
-	imageUrl: string;
-	annotationFile: string;
-}
-
-export interface SheetWithAnnotations extends Sheet {
-	annotations: Annotation[];
-}
-
 export function makeId(): string {
 	if (globalThis.crypto?.randomUUID) {
 		return globalThis.crypto.randomUUID();
